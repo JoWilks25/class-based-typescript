e1d82bae0ee2a58f1c334f7292c63ebf62f63aa1
Jo Wilks <jowilks@Jos-MacBook-Pro.local>
2023-04-15 16:33:10 +0100

    Cleanup types

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,9 +1,16 @@
 import * as types from '../constants/ActionTypes'
 
-export const addTodo = (text: string) => ({ type: types.ADD_TODO, text })
-export const deleteTodo = (id: number) => ({ type: types.DELETE_TODO, id })
-export const editTodo = (id: number, text: string) => ({ type: types.EDIT_TODO, id, text })
-export const completeTodo = (id: number) => ({ type: types.COMPLETE_TODO, id })
-export const completeAllTodos = () => ({ type: types.COMPLETE_ALL_TODOS })
-export const clearCompleted = () => ({ type: types.CLEAR_COMPLETED })
-export const setVisibilityFilter = (filter: string) => ({ type: types.SET_VISIBILITY_FILTER, filter })
+interface actionType {
+  type: string;
+  text?: string;
+  id?: number;
+  filter?: string;
+}
+
+export const addTodo = (text: string): actionType => ({ type: types.ADD_TODO, text })
+export const deleteTodo = (id: number): actionType => ({ type: types.DELETE_TODO, id })
+export const editTodo = (id: number, text: string): actionType => ({ type: types.EDIT_TODO, id, text })
+export const completeTodo = (id: number): actionType => ({ type: types.COMPLETE_TODO, id })
+export const completeAllTodos = (): actionType => ({ type: types.COMPLETE_ALL_TODOS })
+export const clearCompleted = (): actionType => ({ type: types.CLEAR_COMPLETED })
+export const setVisibilityFilter = (filter: string): actionType => ({ type: types.SET_VISIBILITY_FILTER, filter })
